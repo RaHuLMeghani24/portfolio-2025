831da8a0126715b7b40eb7602b522f47dc0929f5
Rahul Meghani <rahulmeghani36@gmail.com>
2025-07-06 15:57:20 -0400

    Sorting out the import statements and adding a Navigation bar to the portfolio.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Hero } from './components/sections/Hero';
+import Hero from './components/sections/Hero';
+import ShowcaseSection from './components/sections/ShowcaseSection';
+import LogoShowcase from './components/sections/LogoShowcase';
+import FeatureCards from './components/sections/FeatureCards';
+import Experience from './components/sections/Experience';
+import NavBar from './components/NavBar';
 
 
 const App = () => {
   return (
     <>
+      <NavBar/>
       <Hero />
+      <ShowcaseSection />
+      <LogoShowcase />
+      <FeatureCards />
+      <Experience />
     </>
   )
 }
